1b6a1b75d9c55a77508e3de1e04f610714fe0eca
Hyperflip <andert.philipp@gmail.com>
2019-09-21 15:46:07 +0200

    Unit tests
    implemented Unit tests and added TODO to UEB1

---
+++ src/BIF/SWE1/UEB1.java
@@ -0,0 +1,14 @@
+package BIF.SWE1;
+
+import BIF.SWE1.interfaces.Url;
+
+public class UEB1 {
+
+	public Url getUrl(String path) {
+		// Url url = new Url(path); TODO define Url interface and return empty object
+		return null;
+	}
+
+	public void helloWorld() {
+	}
+}
