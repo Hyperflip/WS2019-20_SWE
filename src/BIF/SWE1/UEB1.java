a189bc2d059d7e8880e5fc56dff90c3d5827cac9
Hyperflip <andert.philipp@gmail.com>
2019-10-28 20:21:47 +0100

    Implemented UrlFactory class which parses the url. Contains getWebUrl() function that returns a webUrl object with the parsed url. UEB1 now creates UrlFactory object and returns Url object from getWebUrl() All UEB1Test tests passed succesfully.

--- src/BIF/SWE1/UEB1.java
+++ src/BIF/SWE1/UEB1.java
@@ -5,8 +5,7 @@ import BIF.SWE1.interfaces.Url;
 public class UEB1 {
 
 	public Url getUrl(String path) {
-		// Url url = new Url(path); TODO define Url interface and return empty object
-		return null;
+		return new UrlFactory().getWebUrl(path);
 	}
 
 	public void helloWorld() {
